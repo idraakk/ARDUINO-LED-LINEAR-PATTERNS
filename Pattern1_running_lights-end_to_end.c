1391628e8eb224f6918469ace69216b8743791fb
Idraak Mohd Khan <idraakmk@gmail.com>
2023-11-19 17:34:42 +0530

    Create Pattern1_running_lights-end_to_end.c

---
+++ Pattern1_running_lights-end_to_end.c
@@ -0,0 +1,41 @@
+void setup() {
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
+
+}
+void loop() 
+{
+  //PATTERM 1 RUNNING LIGHTS END TO END
+  //LIGHT ON
+  for(int i=12;i>=3;i--)
+  {
+    for(int j=3;j<=i;j++)
+    {
+      digitalWrite(j,HIGH);
+      delay(25);
+      digitalWrite(j,LOW);
+    }
+    digitalWrite(i,HIGH);
+    delay(200);
+  }
+  //LIGHT OFF
+  for(int i=3;i<=12;i++)
+  {
+    for(int j=12;j>=i;j--)
+    {
+      digitalWrite(j,LOW);
+      delay(25);
+      digitalWrite(j,HIGH);
+    }
+    digitalWrite(i,LOW);
+    delay(200);
+  }
+}
